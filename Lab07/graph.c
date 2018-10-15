0cc6b2d2113a387a36197604e9227193bb3c6f3c
Keith Skinner <keithskinner93@gmail.com>
2018-10-15 05:15:54 -0700

    done with lab 07

--- Lab07/graph.c
+++ Lab07/graph.c
@@ -1,49 +1,74 @@
+/**
+* Name: Keith Skinner
+* Lab/task: Lab 7
+* Date: 10/15/2018
+**/
+
 #include "graph.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-bool ** makeTable(int size)
+int ** makeTable(int size)
 //creates and returns a 2-D Square table of size verts
 {
-    bool ** table = calloc(size, sizeof(bool*));
+    const size_t len = (size_t)size;
+    int ** table = calloc(len, sizeof(int*));
     for (int i=0; i<size; ++i)
-        table[i] = calloc(size, sizeof(bool));
+        table[i] = calloc(len, sizeof(int));
     return table;
 }
 
 
-bool cycle(bool ** table, const int size, int vertex, bool* visited)
+void disconnect(int ** table, const int size, int vertex)
+//helper function for searchForCycle that removes any
+// connections to a node that is proven to have no loop
+{
+    for (int i = 0; i<size; ++i) {
+        table[i][vertex] = 0;
+        table[vertex][i] = 0;
+    }
+}
+
+
+bool cycle(int ** table, const int size, int vertex, bool* visited)
+//helper function for searchForCycle.
 //Uses depth first search to see if the vertex, row vertex is involved in a cycle.
 {
+    bool loop = false;
     visited[vertex] = true;
     for (int i=0; i<size; ++i) {
-        if (table[vertex][i]) { //connected
-            return (visited[i])
-                   ? true
-                   : cycle(table, size, i, visited);
+        if (table[vertex][i] != 0) {                        //connected
+            if (!visited[i]) {                              //not yet visited!
+                if (cycle(table, size, i, visited))         //is this connected node part of a cycle?
+                    return true;                            //it is directly or indirectly part of a loop.
+            }
+            else loop = true;                               //connected and seen before! must be a loop!
         }
     }
-    return false;
+    disconnect(table, size, vertex);                        //this vertex has proven that it and
+                                                            // it's connections are not part of a loop
+    return loop;
 }
 
-void disconnect(bool ** table, const int size, int vertex)
-{
-    for (int i = 0; i<size; ++i)
-        table[vertex][i] = false;
-}
 
-bool searchForCycle(bool ** table, int size)
+bool searchForCycle(int ** table, int size)
 //Searches each vertex to see if it's involved with a deadlock
 {
     bool *visited = calloc( (size_t)size, sizeof(bool) );
-
     for (int vertex = 0; vertex < size; ++vertex) {
         if (!visited[vertex] && cycle(table, size, vertex, visited)) {
             free(visited);
             return true;
         }
-        disconnect(table, size, vertex);
     }
-
     free(visited);
     return false;
+}
+
+void deleteTable(int ** table, int size)
+{
+    for (--size; size >= 0; --size)
+        free(table[size]);
+    free(table);
 }
--- Lab07/main.c
+++ Lab07/main.c
@@ -1,42 +1,142 @@
+/**
+* Name: Keith Skinner
+* Lab/task: Lab 7
+* Date: 10/15/2018
+**/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "graph.h"
 
-#define MAX_LINE_LENGTH BUFSIZ
-#define MAX_NAME_LENGTH BUFSIZ/8
+#define MAX_LINE_LENGTH 256
 
-typedef struct {
-    int from;
-    int to;
-} EDGE;
-
-int countVerticies(char * buffer)
+char * getOffset(char * line, char * next)
 {
-    int count = 0;
-    while(strchr(buffer, ',') != NULL)
-        ++count;
-    return count + 1;
+    int offset = 0;
+    if (sscanf(line, "%[^,{} ]%n", next, &offset) == 0)
+        offset = 1;
+    return line += offset;
 }
 
-char ** getVerticies(char * buffer, const int length)
+char ** addVertex(char * next, char ** vertices, int * numVertices)
 {
-    int count = countVerticies(buffer);
-    char
+    *numVertices += 1;
+    vertices = realloc(vertices, (*numVertices) * sizeof(char*));
+    vertices[(*numVertices)-1] = malloc(strlen(next)+1);
+    strcpy(vertices[(*numVertices)-1], next);
+    return vertices;
+}
 
+
+char ** getVertices(char * line, int * numVertices)
+{
+    char ** vertices = NULL;
+    *numVertices = 0;
+
+    int offset = 0;
+    sscanf(line, " V = { %n", &offset);
+    line += offset;
+
+    char next[16];
+    while (*line != '\0') {
+        line = getOffset(line, next);
+        if (next[0] != '\0')
+            vertices = addVertex(next, vertices, numVertices);
+        next[0] = '\0';
+    }
+    return vertices;
+}
+
+char * addConnection(char * next, char *connection)
+{
+    connection = realloc(connection, strlen(next)+1);
+    strcpy(connection, next);
+    return connection;
+}
+
+bool getConnection(char ** line, char * next, char *connection[2])
+{
+    int vertex = 0;
+    while (vertex<2 && (*line)[0] != '\0') {
+        *line = getOffset(*line, next);
+        if (next[0] != '\0'){
+            connection[vertex] = addConnection(next, connection[vertex]);
+            ++vertex;
+        }
+        next[0] = '\0';
+    }
+    return vertex == 2;
+}
+
+int findVertex(char ** vertices, int size, char * vertex)
+{
+    int i=0;
+    for (; i<size; ++i)
+        if (strcmp(vertices[i], vertex) == 0)
+            break;
+    return i;
+}
+
+void setConnections(int ** table, char ** vertices, int size, char * line)
+{
+    //so I can safely realloc
+    char * connection[2] = {
+            calloc(0, sizeof(char)),
+            calloc(0, sizeof(char))
+    };
+
+    int offset = 0;
+    sscanf(line, " E = { %n", &offset);
+    line += offset;
+
+    char next[16] = "";
+    while(getConnection(&line, next, connection)) {
+        int from = findVertex(vertices, size, connection[0]);
+        int to = findVertex(vertices, size, connection[1]);
+        table[from][to] = 1;
+    }
+
+    free(connection[0]);
+    free(connection[1]);
+}
+
+void output(int ** table, int size)
+{
+    printf("GRAPH ADJACENCY MATRIX\n\n");
+
+    for (int row=0; row<size; ++row){
+        for (int col=0; col<size; ++col)
+            printf("%3d",table[row][col]);
+        printf("\n");
+    }
+    printf("\n");
+
+    searchForCycle(table, size)
+        ? printf(">> DEADLOCKED <<\n")
+        : printf(">> NO DEADLOCK <<\n");
+}
+
+void outputVertices(char ** vertices, int numVertices)
+{
+    printf("\nverts: %d\n", numVertices);
+    for (int i=0; i<numVertices; ++i)
+        printf("%-5s", vertices[i]);
+    printf("\n");
 }
 
 int main()
 {
-    char buffer[MAX_LINE_LENGTH] = "";
-    char ** verticies = getVerticies(buffer, MAX_LINE_LENGTH);
-    //char ** edges = getEdges();
+    char line[MAX_LINE_LENGTH] = "";
+    scanf("%[^\n]\n", line);
 
-    scanf("V={%s}\n");
-    scanf("E={%s}\n");
-        scanf("{%s, %s}")
+    int numVertices = 0;
+    char ** vertices = getVertices(line, &numVertices);
+    int ** table = makeTable(numVertices);
+    scanf("%[^\n]", line);
+    setConnections(table, vertices, numVertices, line);
 
-
-
-    return 0;
+    //outputVertices(vertices, numVertices);
+    output(table, numVertices);
 }
