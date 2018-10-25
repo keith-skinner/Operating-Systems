91f0b98bb4662c0f8ee5fd5c279a4f057146a7f1
Keith Skinner <brian.skinner121@myci.csuci.edu>
2018-10-25 15:46:50 -0700

    DONE just do output

--- Lab09/replace.c
+++ Lab09/replace.c
@@ -7,23 +7,128 @@
 #include "replace.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+//questionable functions
+bool isTail(PAGE * page) {
+    return page->next == NULL;
+}
+bool isHead(PAGE * page) {
+    return page->prev == NULL;
+}
 
 
-
-void print_pages(PAGE * pages, int capacity, int size)
+void print_pages(PAGE * pages)
 {
-    for (--size; size<=0; --size, --capacity)
-        printf("%d ", size);
-    for (; capacity<=0; --capacity)
-        printf("  ");
+    while (pages != NULL) {
+        printf("%-3ld", pages->page);
+        pages = pages->next;
+    }
     printf("\n");
+}
+
+
+PAGE * findPage(PAGE * pages, long page)
+{
+    while (pages != NULL) {
+        if (pages->page == page)
+            return pages;
+        else pages = pages->next;
+    }
+    return NULL;
+}
+
+
+long getNextPage(char * line, char ** pos)
+{
+    long value = strtol(line, pos, 10);
+    return (*pos != line) ? value : -1;
+}
+
+void addPage(PAGE ** head, PAGE ** tail, long page, long * size)
+{
+    //construct new head ptr
+    PAGE * newTop = malloc(sizeof(PAGE));
+    newTop->page = page;
+    newTop->next = *head;
+    newTop->prev = NULL;
+    //put newTop into empty list
+    if (*head == NULL) {
+        *head = newTop;
+        *tail = newTop;
+    }
+    //put newTop into non-empty list
+    else {
+        (*head)->prev = newTop;
+        *head = newTop;
+    }
+    //increase size
+    ++(*size);
+}
+
+void movePage(PAGE ** head, PAGE ** tail, PAGE * referenced, long page)
+{
+    if (!isHead(referenced)) {
+        //need to adjust tail ptr if moving element is tail.
+        if (isTail(referenced))
+            *tail = referenced->prev;
+        //move pointers.
+        referenced->prev->next = referenced->next;
+        if (!isTail(referenced))
+            referenced->next->prev = referenced->prev;
+        (*head)->prev = referenced;
+        referenced->next = *head;
+        referenced->prev = NULL;
+        *head = referenced;
+    }
+    (*head)->page = page;
+}
+
+void pageReference(PAGE ** head, PAGE ** tail, PAGE * referenced, long page)
+{
+    putchar('>');
+    movePage(head, tail, referenced, page);
+}
+
+void pageFault(PAGE ** head, PAGE ** tail, long page, long capacity, long * size)
+{
+    putchar('*');
+    if (*size < capacity)
+        addPage(head, tail, page, size);
+    else movePage(head, tail, *tail, page);
 }
 
 int main(int argc, char ** argv)
 {
-    freopen(argv[1],"r", stdin);
-    PAGE * pages = NULL;
-    int size = 0;
+    freopen(argv[1], "r", stdin);
+    char line[MAX_LINE_LEN] = "";
+    scanf("%[^\n]\n", line);
 
+    PAGE * head = NULL;
+    PAGE * tail = NULL;
+    long capacity = strtol(line, NULL, 10);
+    long size = 0;
 
+    PAGE * referenced = NULL;
+    long faults = 0;
+
+    putchar(' ');
+    print_pages(head);
+
+    scanf("%[^\n]\n", line);
+    char * pos = line;
+    long page = getNextPage(pos, &pos);
+    while (page != -1)
+    {
+        referenced = findPage(head, page);
+        if (referenced == NULL) {
+            pageFault(&head, &tail, page, capacity, &size);
+            ++faults;
+        }
+        else
+            pageReference(&head, &tail, referenced, page);
+        print_pages(head);
+        page = getNextPage(pos, &pos);
+    }
+    printf("\nNumber of faults: %ld\n", faults);
 }
--- Lab09/replace.h
+++ Lab09/replace.h
@@ -9,10 +9,20 @@
 
 #define MAX_LINE_LEN 256
 
-typedef struct page_tag{
+typedef struct page_tag
+{
     struct page_tag * prev;
     struct page_tag * next;
-    int page;
+    long page;
 } PAGE;
+
+
+PAGE * findPage(PAGE * pages, long page);
+void print_pages(PAGE * pages);
+long getNextPage(char * line, char ** pos);
+void pageFault(PAGE ** head, PAGE ** tail, long page, long capacity, long * size);
+void pageReference(PAGE ** head, PAGE ** tail, PAGE * referenced, long page);
+void movePage(PAGE ** head, PAGE ** tail, PAGE * referenced, long page);
+void addPage(PAGE ** head, PAGE ** tail, long page, long * size);
 
 #endif //LAB09_REPLACE_H
