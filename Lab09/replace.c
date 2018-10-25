08b2c5398f0fb55ac9cb01256109b0a6c8492aeb
Keith Skinner <brian.skinner121@myci.csuci.edu>
2018-10-25 12:37:38 -0700

    lab instructions

--- Lab09/replace.c
+++ Lab09/replace.c
@@ -4,21 +4,16 @@
 * Date: 10//29/2018
 **/
 
+#include "replace.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LINE_LEN 256
 
-typedef struct page_tag{
-    struct page_tag * prev;
-    struct page_tag * next;
-    int page;
-} PAGE;
 
 void print_pages(PAGE * pages, int capacity, int size)
 {
     for (--size; size<=0; --size, --capacity)
-        printf("%d ", pages->page);
+        printf("%d ", size);
     for (; capacity<=0; --capacity)
         printf("  ");
     printf("\n");
--- Lab09/replace.h
+++ Lab09/replace.h
@@ -3,3 +3,16 @@
 * Lab/task: Lab 9
 * Date: 10//29/2018
 **/
+
+#ifndef LAB09_REPLACE_H
+#define LAB09_REPLACE_H
+
+#define MAX_LINE_LEN 256
+
+typedef struct page_tag{
+    struct page_tag * prev;
+    struct page_tag * next;
+    int page;
+} PAGE;
+
+#endif //LAB09_REPLACE_H
